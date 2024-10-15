d3d30d5ae93ca19b46a08adc125445619c78ece2
Raghavendra <156374725+Raghavp123@users.noreply.github.com>
2024-10-15 16:23:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,29 @@
 import streamlit as st
+from openai import OpenAI
+import os
 
+### Load your API Key
+my_secret_key = userdata.get('MyOpenAIKey')
+os.environ["OPENAI_API_KEY"] = my_secret_key
+
+### Request the answer to the question "Damascus is a"
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": "Damascus is a"}
+  ],
+  seed = BUID,
+  n=10,
+  max_tokens=20
+)
+
+### Print all 10 completions:
+for i in range(10):
+    st.write(response.choices[i].message.content)
+  ##print(response.choices[i].message.content)
+    
 st.title("🎈 My latest app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
